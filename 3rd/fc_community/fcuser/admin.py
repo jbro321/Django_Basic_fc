e9f1f46825f937b8cbd5cb317755568ae1924c40
JP <jaehyung0321@gmail.com>
2021-06-09 11:26:23 +0900

    update admin.py

--- 3rd/fc_community/fcuser/admin.py
+++ 3rd/fc_community/fcuser/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Fcuser
 
 # Register your models here.
+
+class FcuserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password') # 이름과 패스워드로 카테고리 나오게한다.
+
+admin.site.register(Fcuser, FcuserAdmin)
