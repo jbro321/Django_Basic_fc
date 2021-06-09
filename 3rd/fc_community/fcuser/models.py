4ab76debc71653673af2bfae5671ab09a15f3c6c
JP <jaehyung0321@gmail.com>
2021-06-09 11:26:42 +0900

    update models.py

--- 3rd/fc_community/fcuser/models.py
+++ 3rd/fc_community/fcuser/models.py
@@ -11,6 +11,12 @@ class Fcuser(models.Model):
     registered_dttm = models.DateTimeField(auto_now_add=True, #dttm은 데이트타임의 약자
                                            verbose_name='등록시간')
 
+    def __str__(self): #Fcuser의 이름을 이름으로 나오게 함
+        return self.username
+
     class Meta: #테이블명 직접 지정
         db_table = 'fastcampus_fcuser'
+        verbose_name = '패스트캠퍼스 사용자' # Fcuser 클래스를 '패스트캠퍼스'로 지정한다.
+        verbose_name_plural = '패스트캠퍼스 사용자' # 기본적으로 복수형으로 나타난다. 's'가 붙어서 나오는걸 없애기
+        
         
