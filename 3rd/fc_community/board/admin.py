f4fd1d35fe46f903e4c1a01c3b766e12223c528d
JP <jaehyung0321@gmail.com>
2021-06-22 11:12:25 +0900

    Django_Basic_fc

--- 3rd/fc_community/board/admin.py
+++ 3rd/fc_community/board/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Board
 
 # Register your models here.
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('title',) # 이름과 패스워드로 카테고리 나오게한다.
+
+admin.site.register(Board, BoardAdmin)
--- 3rd/fc_community/board/models.py
+++ 3rd/fc_community/board/models.py
@@ -5,17 +5,17 @@ from django.db import models
 class Board(models.Model):
     title = models.CharField(max_length=128,
                                 verbose_name='제목')
-    contents = models.TextField(max_length=128, # emailfield는 그 문자열이 email형태를 검증해준다.
-                                verbose_name='사용자이메일')
-    writer = models.CharField(max_length=64,
-                                verbose_name='비밀번호')
+    contents = models.TextField(max_length=128, # textfield 길이제한없는 걸로 사용
+                                verbose_name='내용')
+    writer = models.ForeignKey('fcuser.Fcuser', # id로 연결하는 키
+                                on_delete=models.CASCADE, verbose_name='작성자')
     registered_dttm = models.DateTimeField(auto_now_add=True, #dttm은 데이트타임의 약자
                                            verbose_name='등록시간')
 
     def __str__(self): #Fcuser의 이름을 이름으로 나오게 함
-        return self.username
+        return self.title
 
     class Meta: #테이블명 직접 지정
-        db_table = 'fastcampus_fcuser'
-        verbose_name = '패스트캠퍼스 사용자' # Fcuser 클래스를 '패스트캠퍼스'로 지정한다.
-        verbose_name_plural = '패스트캠퍼스 사용자' # 기본적으로 복수형으로 나타난다. 's'가 붙어서 나오는걸 없애기
+        db_table = 'fastcampus_board'
+        verbose_name = '패스트캠퍼스 게시글' # Fcuser 클래스를 '패스트캠퍼스'로 지정한다.
+        verbose_name_plural = '패스트캠퍼스 게시글' # 기본적으로 복수형으로 나타난다. 's'가 붙어서 나오는걸 없애기
